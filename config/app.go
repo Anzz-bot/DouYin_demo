2182a4d1f51702401e7772fd2691135896f564a0
小龙 黄 <3365826446@qq.com>
2022-05-18 18:26:25 +0800

    [fix]1min Port global

--- config/app.go
+++ config/app.go
@@ -8,7 +8,7 @@ package config
 
 type App struct {
 	Env     string `mapstructure:"env" json:"env" yaml:"env"`
-	port    string `mapstructure:"port" json:"port" yaml:"port"`
+	Port    string `mapstructure:"port" json:"port" yaml:"port"`
 	AppName string `mapstructure:"app_name" json:"app_name" yaml:"app_name"`
 	AppUrl  string `mapstructure:"app_url" json:"app_url" yaml:"app_url"`
 }
