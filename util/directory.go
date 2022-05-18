e0c26f13139152419586ed1ff3b7e62aa7dbe751
小龙 黄 <3365826446@qq.com>
2022-05-18 19:23:31 +0800

    [init]30min add judge path func

---
+++ util/directory.go
@@ -0,0 +1,21 @@
+/*
+ * @Author: alexander.huang
+ * @Date:   2022-05-18 19:10:11
+ * @Last Modified by: alexander.huang
+ * @Last Modified time: 2022-05-18 19:10:11
+ */
+package util
+
+import "os"
+
+// judge path exist
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, err
+	}
+	return false, err
+}
